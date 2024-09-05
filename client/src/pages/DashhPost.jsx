ecbca5bec7876558f696aa11930449b6be8feecf
Muhammad-Hassan-student <akramhassan4445@gmail.com>
2024-09-05 18:25:26 +0500

    Add show more functionality to the posts results of the dashboard

--- client/src/pages/DashhPost.jsx
+++ client/src/pages/DashhPost.jsx
@@ -6,15 +6,18 @@ import { Link } from "react-router-dom";
 export default function DashhPost() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+  const [showMore,setShowMore] =useState(true);
   //get post
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`/api/v1/post/getPosts`);
+        const res = await fetch(`/api/v1/post/getPosts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error);
@@ -24,6 +27,21 @@ export default function DashhPost() {
       fetchPosts();
     }
   }, [currentUser._id]);
+  const handleShowMore= async () => {
+    const startIndex= userPosts.length;
+    try {
+      const res = await fetch(`/api/v1/post/getPosts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data= await res.json();
+      if(res.ok){
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if(data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -71,6 +89,9 @@ export default function DashhPost() {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show More</button>
+          )}
         </>
       ) : (
         <h1>You have no posts yet</h1>
