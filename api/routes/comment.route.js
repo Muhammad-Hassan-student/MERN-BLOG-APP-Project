721c40fda11df98cd5c6d83fb1eceb63432827a0
Muhammad-Hassan-student <akramhassan4445@gmail.com>
2024-10-03 22:03:37 +0500

    Add delete functionality to the comment section

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
+  deleteComment,
   editComment,
   getComments,
   likeComment,
@@ -16,4 +17,6 @@ router.get("/getComments/:postId", getComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
 
 router.put("/editComment/:commentId", verifyToken, editComment);
+
+router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,11 +4,11 @@ import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
 
-export default function Comment({ comment, onLike,onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
   const [user, setUser] = useState({});
   const { currentUser } = useSelector((state) => state.user);
-  const [isEditing,setIsEditing]= useState(false);
-  const [editContent,setEditContent] =useState(comment.content);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editContent, setEditContent] = useState(comment.content);
 
   console.log(comment._id);
 
@@ -27,20 +27,20 @@ export default function Comment({ comment, onLike,onEdit }) {
     getUser();
   }, [comment]);
 
-  const handleEdit=async () => {
+  const handleEdit = async () => {
     setIsEditing(true);
     setEditContent(comment.content);
-  }
+  };
   const handleSave = async () => {
     try {
-      const res  = await fetch(`/api/v1/comment/editComment/${comment._id}`, {
-        method: 'PUT',
+      const res = await fetch(`/api/v1/comment/editComment/${comment._id}`, {
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          content: editContent
-        })
+          content: editContent,
+        }),
       });
       if (res.ok) {
         setIsEditing(false);
@@ -49,7 +49,7 @@ export default function Comment({ comment, onLike,onEdit }) {
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
       <div className="flex-shrink-0 mr-3">
@@ -70,46 +70,73 @@ export default function Comment({ comment, onLike,onEdit }) {
         </div>
         {isEditing ? (
           <>
-             <Textarea
-             className="mb-2"
-            placeholder="Add a comment..."
-            rows={"3"}
-            maxLength={"200"}
-            onChange={(e) =>  setEditContent(e.target.value)}
-            value={editContent}
-          />
-          <div className="flex justify-end gap-2 text-xs">
-            <Button gradientDuoTone={'greenToBlue'} type="button" size={'sm'} onClick={handleSave}>Save</Button>
-            <Button gradientDuoTone={'pinkToOrange'} size={'sm'} outline onClick={() => setIsEditing(false)}>Cancel</Button>
-          </div>
+            <Textarea
+              className="mb-2"
+              placeholder="Add a comment..."
+              rows={"3"}
+              maxLength={"200"}
+              onChange={(e) => setEditContent(e.target.value)}
+              value={editContent}
+            />
+            <div className="flex justify-end gap-2 text-xs">
+              <Button
+                gradientDuoTone={"greenToBlue"}
+                type="button"
+                size={"sm"}
+                onClick={handleSave}
+              >
+                Save
+              </Button>
+              <Button
+                gradientDuoTone={"pinkToOrange"}
+                size={"sm"}
+                outline
+                onClick={() => setIsEditing(false)}
+              >
+                Cancel
+              </Button>
+            </div>
           </>
         ) : (
           <>
-              <p className="text-gray-500 pb-2">{comment.content}</p>
-        <div className="flex items-center pt-2 border-t text-xs dark:border-gray-700 max-w-fit gap-2">
-          <button
-            className={`text-gray-400 hover:text-blue-500 ${
-              currentUser &&
-              comment.likes.includes(currentUser._id) &&
-              "!text-blue-500"
-            }`}
-            onClick={() => onLike(comment._id)}
-          >
-            <FaThumbsUp />
-          </button>
-          <p className="text-gray-400">
-            {comment.numberOfLikes > 0 &&
-              comment.numberOfLikes +
-                " " +
-                (comment.numberOfLikes === 1 ? "Like" : "Likes")}
-          </p>
-          {currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-            <button className="text-gray-400 hover:text-blue-500" onClick={handleEdit}>Edit</button>
-          )}
-        </div>
+            <p className="text-gray-500 pb-2">{comment.content}</p>
+            <div className="flex items-center pt-2 border-t text-xs dark:border-gray-700 max-w-fit gap-2">
+              <button
+                className={`text-gray-400 hover:text-blue-500 ${
+                  currentUser &&
+                  comment.likes.includes(currentUser._id) &&
+                  "!text-blue-500"
+                }`}
+                onClick={() => onLike(comment._id)}
+              >
+                <FaThumbsUp />
+              </button>
+              <p className="text-gray-400">
+                {comment.numberOfLikes > 0 &&
+                  comment.numberOfLikes +
+                    " " +
+                    (comment.numberOfLikes === 1 ? "Like" : "Likes")}
+              </p>
+              {currentUser &&
+                (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                  <>
+                    <button
+                      className="text-gray-400 hover:text-blue-500"
+                      onClick={handleEdit}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="text-gray-400 hover:text-red-500"
+                      onClick={() => onDelete(comment._id)}
+                    >
+                      Delete
+                    </button>
+                  </>
+                )}
+            </div>
           </>
         )}
-      
       </div>
     </div>
   );
