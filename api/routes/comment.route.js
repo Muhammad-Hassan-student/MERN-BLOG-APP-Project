41d6a640e717360ff0e7d57d3605c3eff44fcbc1
Muhammad-Hassan-student <akramhassan4445@gmail.com>
2024-10-03 21:01:24 +0500

    Add edit functionalty to the comment component

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
+  editComment,
   getComments,
   likeComment,
 } from "../controller/comment.controller.js";
@@ -13,4 +14,6 @@ router.post("/create", verifyToken, createComment);
 router.get("/getComments/:postId", getComments);
 
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+
+router.put("/editComment/:commentId", verifyToken, editComment);
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import moment from "moment";
 import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { Button, Textarea } from "flowbite-react";
 
-export default function Comment({ comment, onLike }) {
+export default function Comment({ comment, onLike,onEdit }) {
   const [user, setUser] = useState({});
   const { currentUser } = useSelector((state) => state.user);
+  const [isEditing,setIsEditing]= useState(false);
+  const [editContent,setEditContent] =useState(comment.content);
 
-  console.log(user);
+  console.log(comment._id);
 
   useEffect(() => {
     const getUser = async () => {
@@ -23,6 +26,30 @@ export default function Comment({ comment, onLike }) {
     };
     getUser();
   }, [comment]);
+
+  const handleEdit=async () => {
+    setIsEditing(true);
+    setEditContent(comment.content);
+  }
+  const handleSave = async () => {
+    try {
+      const res  = await fetch(`/api/v1/comment/editComment/${comment._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          content: editContent
+        })
+      });
+      if (res.ok) {
+        setIsEditing(false);
+        onEdit(comment, editContent);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
       <div className="flex-shrink-0 mr-3">
@@ -41,8 +68,25 @@ export default function Comment({ comment, onLike }) {
             {moment(comment.createdAt).fromNow()}
           </span>
         </div>
-        <p className="text-gray-500 pb-2">{comment.content}</p>
-        <div className="flex items-center pt-2 border-t border-gray-700 max-w-fit gap-2">
+        {isEditing ? (
+          <>
+             <Textarea
+             className="mb-2"
+            placeholder="Add a comment..."
+            rows={"3"}
+            maxLength={"200"}
+            onChange={(e) =>  setEditContent(e.target.value)}
+            value={editContent}
+          />
+          <div className="flex justify-end gap-2 text-xs">
+            <Button gradientDuoTone={'greenToBlue'} type="button" size={'sm'} onClick={handleSave}>Save</Button>
+            <Button gradientDuoTone={'pinkToOrange'} size={'sm'} outline onClick={() => setIsEditing(false)}>Cancel</Button>
+          </div>
+          </>
+        ) : (
+          <>
+              <p className="text-gray-500 pb-2">{comment.content}</p>
+        <div className="flex items-center pt-2 border-t text-xs dark:border-gray-700 max-w-fit gap-2">
           <button
             className={`text-gray-400 hover:text-blue-500 ${
               currentUser &&
@@ -59,7 +103,13 @@ export default function Comment({ comment, onLike }) {
                 " " +
                 (comment.numberOfLikes === 1 ? "Like" : "Likes")}
           </p>
+          {currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+            <button className="text-gray-400 hover:text-blue-500" onClick={handleEdit}>Edit</button>
+          )}
         </div>
+          </>
+        )}
+      
       </div>
     </div>
   );
